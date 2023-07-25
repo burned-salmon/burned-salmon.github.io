d895abba35bbeac6b8c77b3a5ae08f7a6b2f291d
Samantha Berne <samanthaberne@gmail.com>
2023-07-24 20:45:31 -0400

    giphy party?

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -4,16 +4,17 @@ $(function () {
     $("form").on("submit", function (event) {
         event.preventDefault();
         const searchVal = $("#search-text").val();
-        addGif(searchVal, 0);
+        //addGif(searchVal);
+        $("#gifs").append("<img src='i-failed-you.gif' style='width:200px;padding:12px;'>");
         $("form").trigger('reset');
     });
 
     $(document).on('click','#btn-remove-gifs',function(){
-        // remove gifs
+        $("#gifs").empty();
     });
 });
 
-async function addGif(search, weirdness) {
+async function addGif(search) {
     let response = await axios.get("api.giphy.com/v1/gifs/search", {params: {q: search, api_key: "IuiMXeF5i949yhWUmAKZ8p9XoxrOj87L", limit: 1}});
     console.log(response.data[0].images.downsized.url);
     //$("#gifs").append(response);
