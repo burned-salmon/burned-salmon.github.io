58384001eac10d4e37f6ed4370ea35e20ae6f34d
Samantha Berne <samanthaberne@gmail.com>
2023-08-07 15:29:20 -0400

    hack or snooze

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -4,18 +4,29 @@ $(function () {
     $("form").on("submit", function (event) {
         event.preventDefault();
         const searchVal = $("#search-text").val();
-        //addGif(searchVal);
-        $("#gifs").append("<img src='i-failed-you.gif' style='width:200px;padding:12px;'>");
+        addGif(searchVal);
+        //$("#gifs").append("<img src='i-failed-you.gif' style='width:200px;padding:12px;'>");
         $("form").trigger('reset');
     });
 
-    $(document).on('click','#btn-remove-gifs',function(){
+    $(document).on('click', '#btn-remove-gifs', function () {
         $("#gifs").empty();
     });
 });
 
 async function addGif(search) {
-    let response = await axios.get("api.giphy.com/v1/gifs/search", {params: {q: search, api_key: "IuiMXeF5i949yhWUmAKZ8p9XoxrOj87L", limit: 1}});
-    console.log(response.data[0].images.downsized.url);
+    const key = "IuiMXeF5i949yhWUmAKZ8p9XoxrOj87L";
+
+    const url = 'http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + key + '&limit=1';
+    axios.get(url)
+        .then((response) => {
+            console.log('success got data', response.data);
+        })
+        .catch((error) => {
+            console.error('An error occurred:', error.message);
+        });
+
+    //let xhr = $.get("http://api.giphy.com/v1/gifs/search", { q: search, api_key: key, limit: 1 });
+    //xhr.done(function (data) { console.log("success got data", data); });
     //$("#gifs").append(response);
 };
